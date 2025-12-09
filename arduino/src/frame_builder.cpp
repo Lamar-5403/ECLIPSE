e936a637cf56728d9b0ce12b6136a9c863385125
Isaiah L Collins <Isaiah.lamar21@gmail.com>
2025-12-08 23:47:57 -0500

    Fixed some layout items

--- arduino/src/frame_builder.cpp
+++ arduino/src/frame_builder.cpp
@@ -4,7 +4,7 @@ void frame_build(frame_t* f, msg_type_t type, const uint8_t* data, uint8_t len) 
     f->start = FRAME_START_BYTE;
     f->type = type;
     f->len = len;
-    for (uint8_t i = 0, i < len, i++)
+    for (uint8_t i = 0; i < len; i++)
         f->payload[i] = data[i];
     f->crc = crc16((uint8_t*)f, 3 + len);       // crc16 file still needs to be implemented
 }

14333794ed731e27f751cd109e1b1d1df52ebf4a
Isaiah L Collins <Isaiah.lamar21@gmail.com>
2026-01-21 16:28:05 -0500

    wifi transport responsibility separation

--- arduino/include/transport_wifi.h
+++ arduino/include/transport_wifi.h
@@ -1,6 +1,14 @@
 #include <cstddef>
 #include <cstdint>
 
+typedef enum {
+    WIFI_UNITIALIZED,
+    WIFI_CONNECTING,
+    WIFI_CONNECTED,
+    WIFI_DISCONNECTED,
+    WIFI_FAILED
+} connection_status_t;
+
 void transport_wifi_init();
 void transport_wifi_poll();
 void transport_wifi_send_byte(const uint8_t*, size_t);
--- arduino/src/transport_wifi.cpp
+++ arduino/src/transport_wifi.cpp
@@ -5,37 +5,78 @@
 #include "frame_decoder.h"
 
 WiFiClient client;
-WiFiServer server(5230);
+connection_status_t connection_status = WIFI_UNITIALIZED;
+
+long connection_attempt_start_time = 0;
 
 void transport_wifi_init() {
+    connection_status_t connection_status = WIFI_CONNECTING;
+
     Serial.begin(115200);
 
     WiFi.begin(SSID, PASS);
 
     Serial.println("Connecting to network...");
 
-    // Wait for connection
-    unsigned long startTime = millis();
-    unsigned long lastCheck = 0;
-    
-    unsigned long now = millis();
-
-    while (WiFi.status() != WL_CONNECTED) {
-
-        if (now - startTime >= 10000) {
-            Serial.println("Could not connect to network.");
-            exit(1);
-        } else if (now - lastCheck >= 250) {
-            lastCheck = now;
-            Serial.println(WiFi.status());
-        }
-    }
-
-    Serial.println("Connected.");
+    // Record timestamp for timeout calculation
+    connection_attempt_start_time = millis();
 }
 
 void transport_wifi_poll() {
+
+    // Switch on connection_status
+    switch (connection_status) {
     
+        case WIFI_CONNECTING:
+            // if connected drain bytes, if not: have we timed out, if not return and wait for connection
+        break;
+    
+        case WIFI_CONNECTED:
+            // check if still connected, if not report dropped connection and return, if so drain bytes and return
+        break;
+
+        default:
+            // system either uninitialized or has not been or is no longer connected, requiring system controller intervention
+            break;
+    }
+
+
+
+
+
+
+
+
+    
+    // On successful connection
+    if (connection_status = WIFI_CONNECTING) {
+        if (WiFi.status() == WL_CONNECTED) {
+            connection_status == WIFI_CONNECTED;
+        }
+    }
+    
+    // Ensure client maintains active connection
+    if (connection_status == WIFI_CONNECTED) {
+        if (WiFi.status() != WL_CONNECTED) {
+            connection_status = WIFI_DISCONNECTED;
+            // Report dropped connection to system controller
+        } else {
+            // Drain available bytes to frame decoder
+            while (client.available() > 0) {
+                frame_decoder_process_byte(client.read());
+            }
+            client.flush();
+            return;
+        }
+    }
+    
+    // Check for connection timeout
+    if (millis() - connection_attempt_start_time >= 10000) {
+        WiFi.disconnect();
+        connection_status = WIFI_FAILED;
+        // Report failure to system controller
+        Serial.print("Network connection attempt timed out.")
+    }    
 }
 
 void transport_wifi_send_byte(const uint8_t*, size_t) {

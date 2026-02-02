5c3d94d9091cd711ad008834e6e7824b7445f148
Isaiah L Collins <Isaiah.lamar21@gmail.com>
2026-02-02 18:31:15 -0500

    Shared module for CRC16 calculation

--- arduino/src/frame_encoder.cpp
+++ arduino/src/frame_encoder.cpp
@@ -3,25 +3,6 @@
 #include "transport_serial.h"
 #include "transport_wifi.h"
 
-uint16_t crc16_ccitt_false(const uint8_t *data, size_t len) {
-    
-    uint16_t crc = 0xFFFF;
-
-    for (uint16_t i = 0; i < len; i++) {
-        crc ^= (uint16_t)data[i] << 8;
-
-        for ( uint8_t bit = 0; bit < 8; bit++) {
-            if (crc & 0x8000) {
-                crc = (crc << 1) ^ 0x1021;
-            } else {
-                crc <<= 1;
-            }
-        }
-    }
-
-    return crc;
-}
-
 void encode_frame(frame_t* f, msg_type_t type, const uint8_t* data, uint8_t len) {
     f->start = FRAME_START_BYTE;
     f->type = static_cast<uint8_t>(type);

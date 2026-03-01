5ed0d51472ce379cbaf6b5638939f89b8363803b
Isaiah Collins <Isaiah.lamar21@gmail.com>
2026-03-01 01:16:32 -0500

    Bug fix

--- pi5/src/frame.py
+++ pi5/src/frame.py
@@ -4,7 +4,7 @@ global FRAME_START_BYTE, FRAME_MAX_PAYLOAD, FRAME_WIRE_SIZE, frame_t, frame_view
 FRAME_START_BYTE = 0xAA
 FRAME_MAX_PAYLOAD = 256
 FRAME_WIRE_SIZE = 1 + 1 + 1 + FRAME_MAX_PAYLOAD + 2
-# frame_t = bytearray(FRAME_WIRE_SIZE)
+frame_t = bytearray(FRAME_WIRE_SIZE)
 # frame_view = memoryview(frame_t)
 
 START_OFS = 0

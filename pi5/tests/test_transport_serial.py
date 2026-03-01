3714030b3bd0caa78e2cb9449ef687148e53ee33
Isaiah Collins <Isaiah.lamar21@gmail.com>
2026-03-01 01:41:33 -0500

    Bug fixes

--- pi5/tests/test_transport_serial.py
+++ pi5/tests/test_transport_serial.py
@@ -18,12 +18,12 @@ frame_encoder.frame_encode(test_frame, frame.msg_type_t.MSG_STATUS_REQUEST, payl
 buf = [0] * (5 + frame.FRAME_MAX_PAYLOAD)
 frame_encoder.calc_serialized_buf(test_frame, buf)
 
+transport_serial.transport_serial_init()
+
 for b in buf[:3 + test_frame[frame.LEN_OFS] + 2]:
     transport_serial.transport_serial_send_byte(b)
 
-while (transport_serial._ser.in_waiting() < 0):
-    pass
-
-transport_serial.transport_serial_poll()
+while True:
+    transport_serial.transport_serial_poll()
 
 # print(f"Frame Received: type: 0x{test_frame[frame.MSG_TYPE_OFS]:02X}, payload = {payload_hex}, CRC: 0x{crc:04X}") 
